c605f6ef39f259644801426a3eea142d77a74b0f
Максим Фагин <Faginm@yandex.ru>
2022-10-24 23:30:41 +0300

    ver 2.0

--- backend/users/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import permissions
-
-
-class CurrentUserReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                and request.user.is_authenticated)
--- backend/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include
-from django.urls import path
-
-urlpatterns = [
-    path('', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
-]
